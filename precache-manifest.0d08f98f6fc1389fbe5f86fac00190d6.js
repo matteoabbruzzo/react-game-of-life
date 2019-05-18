536b198ebc3fc9192958f89702625016fb3a330b
Matteo <matteoabbruzzo@hotmail.it>
2019-05-18 17:20:01 +0200

    Updates

---
+++ precache-manifest.0d08f98f6fc1389fbe5f86fac00190d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d47d4415f215f8863af3a9363d13c186",
+    "url": "/react-game-of-life/static/media/gosper_white.d47d4415.png"
+  },
+  {
+    "revision": "27a80c2f4fa364089445",
+    "url": "/react-game-of-life/static/js/runtime~main.27a80c2f.js"
+  },
+  {
+    "revision": "dce0b6cf354b38e9a851",
+    "url": "/react-game-of-life/static/js/main.dce0b6cf.chunk.js"
+  },
+  {
+    "revision": "31a601f53b4cc8c0f6c6",
+    "url": "/react-game-of-life/static/js/1.31a601f5.chunk.js"
+  },
+  {
+    "revision": "dce0b6cf354b38e9a851",
+    "url": "/react-game-of-life/static/css/main.d8fc441e.chunk.css"
+  },
+  {
+    "revision": "005d1c80d59865a1359010acd9982400",
+    "url": "/react-game-of-life/index.html"
+  }
+];
